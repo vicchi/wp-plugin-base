5627740b54448fbfbf25ab39c6b2fbc26e54d897
vicchi <gary@vicchi.org>
2017-03-27 16:53:14 +0100

    Fix inheritance for multiple classes

--- PluginBase.php
+++ PluginBase.php
@@ -1,24 +1,22 @@
 <?php
 /*
  * Name: PluginBase
- * Description: Abstract base class for developing WordPress plugins; contains helper functions to add WordPress hooks consistently and sanitise hook method names.
- * Version: 2.0.1
+ * Description: Base class for developing WordPress plugins; contains helper functions to add WordPress hooks consistently and sanitise hook method names.
+ * Version: 2.0.2
  * Author: Gary Gale, Travis Smith
  * Author URI: http://www.garygale.com
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Acknowledgements: Based on WPS_Plugin_Base_v1 by Travis Smith (http://wpsmith.net)
+ * Acknowledgements: Based on WP_Plugin_Base_v1 by Travis Smith (http://wpsmith.net)
  */
 
 if (!class_exists('PluginBase')) {
-    abstract class PluginBase {
-        protected static $plugin_name;
+    class PluginBase {
+        protected $plugin_name;
 
         protected function __construct() {
-            isset(self::$plugin_name) or die(__CLASS__ . ': Bad plugin implementation; self::$plugin_name is not set');
+            isset($this->plugin_name) or die(__CLASS__ . ': Bad plugin implementation; $this->plugin_name is not set');
         }
-
-        abstract protected static function get_instance();
 
         protected function hook($hook) {
             $priority = 10;
@@ -37,9 +35,9 @@ if (!class_exists('PluginBase')) {
             return add_action($hook, array ($this, $method), $priority, 999);
         }
 
-        protected static function get_option() {
+        protected function get_option() {
             $num_args = func_num_args();
-            $options = get_option(self::$plugin_name);
+            $options = get_option($this->plugin_name);
 
             if ($num_args > 0) {
                 $args = func_get_args();
@@ -54,14 +52,14 @@ if (!class_exists('PluginBase')) {
             return $options;
         }
 
-        protected static function set_option($key, $value) {
-            $options = get_option(self::$plugin_name);
+        protected function set_option($key, $value) {
+            $options = get_option($this->plugin_name);
             $options[$key] = $value;
-            update_option(self::$plugin_name, $options);
+            update_option($this->plugin_name, $options);
         }
 
-        protected static function update_option($settings) {
-            update_option(self::$plugin_name, $settings);
+        protected function update_option($settings) {
+            update_option($this->plugin_name, $settings);
         }
 
         private function sanitise_method($method) {
